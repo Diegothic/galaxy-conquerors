fac3e4668f19a6f18ad3d6eeb13e263a2f9b9adb
quqacz <67021265+quqacz@users.noreply.github.com>
2022-05-11 08:00:47 +0200

    .

--- core/src/com/galaxy/game/screen/GameScreen.java
+++ core/src/com/galaxy/game/screen/GameScreen.java
@@ -18,6 +18,8 @@ public class GameScreen implements Screen {
     private final GalaxyConquerors game;
     private final OrthographicCamera camera;
     private final GameLevel level;
+    private boolean drawDebug;
+    private boolean debugButtonPressed;
 
     private boolean drawDebug;
     private boolean debugButtonPressed;
