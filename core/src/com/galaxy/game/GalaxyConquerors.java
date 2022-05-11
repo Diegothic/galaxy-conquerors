4411579e77d661626ae075fc23df4032c70451f8
Diegothic <diegothic24@gmail.com>
2022-05-11 10:23:19 +0100

    Fullscreen

--- core/src/com/galaxy/game/GalaxyConquerors.java
+++ core/src/com/galaxy/game/GalaxyConquerors.java
@@ -1,6 +1,8 @@
 package com.galaxy.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.galaxy.game.screen.DevScreen;
@@ -10,16 +12,34 @@ public class GalaxyConquerors extends Game {
     public SpriteBatch batch;
     public ShapeRenderer shapeRenderer;
 
+    private boolean isFullscreen;
+    private boolean fullscreenButtonPressed;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
+        isFullscreen = false;
         this.setScreen(new DevScreen(this));
     }
 
     @Override
     public void render() {
         super.render();
+        handleFullscreen();
+    }
+
+    private void handleFullscreen() {
+        boolean fullscreenButtonWasPressed = fullscreenButtonPressed;
+        fullscreenButtonPressed = Gdx.input.isKeyPressed(Input.Keys.F2);
+        if (fullscreenButtonPressed && !fullscreenButtonWasPressed) {
+            isFullscreen = !isFullscreen;
+            if (isFullscreen) {
+                Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+            } else {
+                Gdx.graphics.setWindowedMode(1280, 720);
+            }
+        }
     }
 
     @Override
